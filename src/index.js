e3166067ae7c6d63d8016d8dcd652edf0c2f42b4
王尚文 <wangshangwen@wangshangwendeMacBook-Pro.local>
2017-07-10 16:33:58 +0800

    flo

--- src/index.js
+++ src/index.js
@@ -1,38 +1,33 @@
+// @flow
 
 import './layout.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { cube  } from './utils';
+import { cube } from './utils';
 
 const box = document.querySelector('#box');
 console.log('loaded3');
-alert(1);
 console.log(cube(3));
 
 class Message extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.name = 'lily';
     }
 
-    render(){
-        return (
-            <h1>{ this.name }</h1>
-        );
+    render() {
+        return ( <h1>{ this.name }</h1> );
     }
 };
 
-
 const aa = {
-    name : 'ke2y1',
-    lily : 'key2'
+    name: 'ke2y1',
+    lily: 'key2'
 };
 
 console.log(Object.values(aa));
 
-ReactDOM.render(<Message />, box);
-
-
+ReactDOM.render( < Message / > , box);
 
 
