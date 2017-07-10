78de2da2a6ff5953127e49f89ea51fe869c7d06a
王尚文 <wangshangwen@wangshangwendeMacBook-Pro.local>
2017-07-10 21:42:49 +0800

    polyfill

--- bin/webpack.base.js
+++ bin/webpack.base.js
@@ -6,7 +6,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: {
-        main: './src/index.js'
+        main: ["babel-polyfill", './src/index.js']
     },
 
     output: {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 import './layout.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {observer} from "mobx-react";
 
 import { cube } from './utils';
 
@@ -27,34 +26,8 @@ const aa = {
     lily: 'key2'
 };
 
-// console.log(Object.values(aa));
+console.log(Object.values(aa));
 
-// ReactDOM.render( < Message / > , box);
+ReactDOM.render( < Message / > , box);
 
-
-@observer class TodoListView extends React.Component {
-    render() {
-        return <div>
-            <ul>
-                {this.props.todoList.todos.map(todo =>
-                    <TodoView todo={todo} key={todo.id} />
-                )}
-            </ul>
-            Tasks left: {this.props.todoList.unfinishedTodoCount}
-        </div>
-    }
-}
-
-const TodoView = observer(({todo}) =>
-    <li>
-        <input
-            type="checkbox"
-            checked={todo.finished}
-            onClick={() => todo.finished = !todo.finished}
-        />{todo.title}
-    </li>
-)
-
-const store = new TodoList();
-ReactDOM.render(<TodoListView todoList={store} />, box);
 
